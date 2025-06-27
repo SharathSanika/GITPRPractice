9affe3e7dcf1d86189bdb4450c45bfc38f106e0a
user <user@DESKTOP-DHSI56N.lan>
2025-06-27 09:30:10 -0700

    TEST

---
+++ src/test/java/Itc/Login.java
@@ -0,0 +1,14 @@
+package Itc;
+
+public class Login {
+	public void m1()
+	{
+		System.out.println("hello login to app");
+	}
+
+	public static void main(String[] args) {
+	Login l=new Login();	
+	l.m1();
+	}
+
+}
